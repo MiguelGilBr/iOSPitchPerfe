840dc9faa94fd006997d5f455a8b3e5d85efb261
Miguel Gil <miguelgil@mac-movilidad.local>
2019-02-04 15:56:19 +0000

    Initial Commit

---
+++ Recorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recorder
+//
+//  Created by Miguel Gil on 04/02/2019.
+//  Copyright © 2019 ldc_tnf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
