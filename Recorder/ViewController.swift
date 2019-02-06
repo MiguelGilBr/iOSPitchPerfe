d3c33447a34ba2a9a7a08aaa70da78249f80701b
Miguel Gil Álvarez <miguel.gila@atos.net>
2019-02-06 11:20:46 +0000

    Added IBActions to Buttons

--- Recorder/ViewController.swift
+++ Recorder/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func startRecord(_ sender: Any) {
+        print("Start button was pressed");
+    }
+    @IBAction func stopRecord(_ sender: Any) {
+        print("Stop button was pressed");
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
